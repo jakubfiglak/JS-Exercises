a259f8dc43519a975e24ba9b4eca88204b06b146
Jakub Figlak <jakub.figlak@gmail.com>
2020-01-27 22:33:55 +0100

    Finish reduce challenge

---
+++ 07-reduce-challange/index.js
@@ -0,0 +1,378 @@
+const text = `
+Szukaj
+SYMPATIA
+ZAKUPY
+GRY
+VOD
+NOWEZNAJDŹ PRACĘ
+E-MAIL
+WIADOMOŚCI
+ 
+SPORT
+ 
+BIZNES
+ 
+REGIONALNE 
+ 
+KULTURA
+ 
+STYL ŻYCIA
+ 
+TECHNOLOGIE
+ 
+MOTORYZACJA
+ 
+PROGRAMY ONETU 
+ 
+...
+75. ROCZNICA WYZWOLENIA AUSCHWITZ-BIRKENAU
+"Nawet się nie obejrzycie, jak na was, »jakiś Auschwitz«, nagle spadnie z nieba"
+
+Byli więźniowie i przywódcy państw oddali hołd ofiarom
+ 
+
+"Wygnano nas o czwartej rano. Padał śnieg. Stałyśmy nago do czwartej po południu"
+
+Ambasador Rosji: bez zwycięstwa ZSRR nie byłoby ani Polski, ani Polaków
+
+Dlaczego Amerykanie i Brytyjczycy nie zrobili nic, by ratować ofiary?
+Wiadomości
+Opowieść wnuka komendanta Auschwitz. "Moja babka nazywała Auschwitz rajem"
+Opowieść wnuka komendanta Auschwitz. "Moja babka nazywała Auschwitz rajem"
+Ten związek nie miał prawa istnieć: ona Żydówka, on oficer SS
+Ten związek nie miał prawa istnieć: ona Żydówka, on oficer SS
+Księżna Kate sfotografowała ocalałych z Holokaustu. "Ich historie pozostaną ze mną"
+Księżna Kate sfotografowała ocalałych z Holokaustu. "Ich historie pozostaną ze mną"
+Co trzeba wiedzieć o obozie Auschwitz? Najważniejsze fakty
+Co trzeba wiedzieć o obozie Auschwitz? Najważniejsze fakty
+Wstrząsająca relacja z Auschwitz. Lekarz do obozu zgłosił się na ochotnika
+Wstrząsająca relacja z Auschwitz. Lekarz do obozu zgłosił się na ochotnika
+"Poczułam ulgę, że dzieci oprawców też cierpią''
+"Poczułam ulgę, że dzieci oprawców też cierpią''
+#WIEM Z ONET
+
+Koronowirus w Chinach sparaliżuje MŚ? "Może będę rekomendował rezygnację Polaków"
+
+Bosacki komentuje wybory w PO. "Jest świetną osobowością medialną"
+#WYBIERAMYPRAWDĘ
+Jedyna kobieta wśród gwałcicieli i pedofilów. "Podglądają mnie. Boję się"
+
+Córka Żołnierzy Wyklętych otrzyma 1 mln zł. Jest wyrok
+
+Kłopoty polskiej delegacji w Radzie Europy. Grozi nam wykluczenie?
+
+Ukraiński miliarder kupił najdroższy dom świata. Zapłacił 220 mln dol.
+
+Na Ukrainie mieszka już mniej ludzi niż w Polsce. Ale też mamy problem
+
+Nowa sondaż: PiS pierwsze, ale poparcie dla partii Kaczyńskiego nadal spada
+
+Z powodu ataków koronawirusa spadną ceny na stacjach. Ropa mocno tanieje
+
+Wielkopolska: radni gminy przyjęli stanowisko przeciwko... LGDP i gender
+
+"Spowiedź agenta Tomka". Jest już reakcja Mariusza Kamińskiego
+
+Szansa na niższe rachunki. Ruszył nabór programu "Mój prąd"
+
+Zwrot akcji ws. Kubicy w DTM? Polak może jednak nie trafić do BMW
+
+Media w USA: zidentyfikowano wszystkie ofiary katastrofy helikoptera Bryanta
+
+Farmy wiatrowe na Bałtyku. Państwowy gigant dostał zielone światło
+
+Słoń Ninio z zoo w Poznaniu u dentysty. Trzeba było użyć wiertarki i dłuta
+
+Chiny obawiają się o swoją gospodarkę. Powodem - epidemia koronowirusa
+WIADOMOŚCI
+Kłopoty Polski w Radzie Europy. Gasiuk-Pihowicz: PiS znów przynosi nam wstyd
+Morawiecki: drugie z piekieł miało na imię Gułag
+KO chce kontroli NIK ws. zakupu samolotów F-35
+Koronawirus w Krakowie? Szpital zaprzecza
+B. więźniarka: to piekło czy dom wariatów? 
+Schramm: do maja trwa walka o życie Dudy 
+Jutro w "Onet Rano." sędzie Waldemar Żurek
+Czy świat jeszcze da się naprawić? 
+Uwaga! TVN: ojciec połamał dzieciom kończyny 
+Ruch prokuratury ws. zeznań agenta Tomka
+Wychodząc z wagonu, poczuła zapach palonych ciał
+Apel sędziów do Dudy o zaniechanie mowy nienawiści
+Uchwała SN. Sędziowie wstrzymują się od orzekania
+Sondaż: PiS zwycięża, ale poparcie spada
+Reakcja Kamińskiego na "spowiedź" agenta Tomka
+Jutro msza święta w intencji KRS
+Provident Polska RRSO 0% na 5mcy REKLAMA
+
+REKLAMA
+
+WIEM WIĘCEJ
+Kilku trafiło na szubienicę, niektórych skazano na dożywocie. Ale sprawiedliwości wymknęło się stanowczo zbyt wielu katów z Auschwitz
+"Duda ułaskawiając skazanych, łamie podstawowe zasady moralne"
+Fryzjerka, która obcina inaczej niż wszystkie. Byliśmy w Ciach Fryzjer
+Największy na świecie odrzutowiec dwusilnikowy zakończył lot próbny. Zobacz nagranie
+We Francji ta metoda pozwoliła na wyjście z nałogu co najmniej 700 tys. ludzi
+Jak dobrze znasz Tatry? Rozwiąż quiz
+Żeby skorzystać z usług Polaka, klienci zza granicy czekają w kolejce nawet trzy lata
+Burza w sieci po rozstaniu Jakuba i Anny z programu "Rolnik szuka żony"
+Znamy wszystkich uczestników "Tańca z gwiazdami". Ostatnie nazwisko zaskakuje
+Ksiądz wysyła listy do parafian. "Dawno nie widziałam takiej pychy"
+Umówili się przez internet: powiedziałam mu tylko, że jestem brzydka
+REKLAMA
+
+NAJLEPSZE PRODUKCJE TV I ONETU
+Player.plIpla.tvProgramy Onetu
+Gdy zobaczył ją trener, załamał ręce. W niecałe dwa miesiące zmieniła się nie do poznania
+19 marca 1945 roku Hitler wydał przerażający rozkaz. Jego wykonanie miałoby straszne konsekwencje
+Rosjanin zaśpiewał piosenkę Zbigniewa Wodeckiego. Jury było w szoku. Niektórzy płakali
+REKLAMA
+
+Pogoda
+Stan powietrza
+Program TV
+Horoskop
+Jak dojadę
+Lotto
+Warszawa 
+DzisiajBezchmurnie1°
+Stan powietrza
+Zły
+PROGNOZA DŁUGOTERMINOWA
+REKLAMA
+
+SPORT
+Ważne słowa byłego pilota Bryanta ws. rozbitej maszyny. "Niewielu ludzi ma do tego odpowiednie CV"
+Gortat: Kobe nie mógł lecieć tak starą maszyną
+Słynny skoczek: Kamil Stoch mnie inspiruje
+NA ŻYWO Puchar Anglii: Arsenal walczy o awans
+Znamy ostatnią ofiarę ze śmigłowca Bryanta
+Wesoła konferencja w Zakopanem, "węgla se kupię"
+Stoch: cieszę się, jakbym wygrał pierwszy raz 
+Doleżal: Kamil Stoch zrobił coś niesamowitego 
+Włoskie media: Zieliński i Insigne utopili Sarriego
+Zwrot akcji ws. Kubicy? Może nie trafić do BMW
+PŚ w skokach: fortuna na koncie Kubackiego
+Śmierć Bryanta. Kontroler lotu polecił zwiększyć wysokość tuż przed tragedią
+Legia dostanie krocie za Majeckiego. To rekord
+"Zieliński grałby w pierwszym składzie Juve"
+Wzruszające oświadczenie Michaela Jordana
+Media: Legia zainteresowana trzecioligowcem
+Brakuje Ci pieniędzy? Wonga.com REKLAMA
+
+Więcej:
+Piłka nożnaSkokiRęcznaSiatkówkaŻużelTenisF1KolarstwoBoksMMAE-sportGorące:
+PŚ w ZakopanemAustralian OpenBundesliga
+REKLAMA
+
+BIZNES
+Koronawirus atakuje rynki na całym świecie. Tracą giganci
+Ukraiński miliarder kupił najdroższy dom na świecie
+Podano ceny biletów do ogromnego parku wodnego w Polsce
+Fundusze Norweskie wspierają Polskę
+UOKiK ma nowego szefa. Jest powołanie premiera
+mBank wytropił powód bierności RPP ws. inflacji
+Jak rozpoznać apartament wyższych standardów
+Koronawirus uderzy w ceny na stacjach. Ropa ostro w dół
+Ogromna kara dla banku Leszka Czarneckiego
+Jest nowy pomysł rządu na emerytury
+Wyprzedaż w Lexusie. Auta z 2019 od 90 tys. zł
+Niskie bezrobocie ma też swoje negatywne oblicze
+Farmy wiatrowe na Bałtyku. Państwowy gigant dostał zielone światło do rozpoczęcia prac
+Więcej:
+Promocja AmazonaCeny nieruchomościGiełdaWalutyFinanseBitcoin
+WALUTY	Kurs	Zmiana
+GIEŁDA	Kurs	Zmiana
+EUR	4,2679	0,51%wzrost
+USD	3,8725	0,65%wzrost
+CHF	3,9915	0,72%wzrost
+WIG	57 063,12	-2,63%spadek
+DJI	28 535,80	-1,57%spadek
+S&P 500	3 243,63	-1,57%spadek
+Gorące: Business Insider Trends Festival
+REKLAMA
+PRZEWIŃ
+WIEM WIĘCEJ
+Do dziś nie wiadomo, kto wskazał Niemcom ich kryjówkę
+Synowa królowej Elżbiety II zastąpi Meghan Markle?
+Wstydliwy problem, który dotyka nawet co drugiego Polaka. Jak sobie z nim poradzić?
+Renata Kaczoruk pokazała się nago. "Przekonałam się, że bez golizny się nie da"
+Sędzia Żurek: w obronę wziął mnie nawet Samuel Pereira z TVP
+Wielkie silniki i ogromna moc - testowaliśmy Vipera, Lambo i SL-a AMG
+QUIZ
+Dopasuj kraj do kontynentu. Tylko mistrz geografii zdobędzie 10/10
+Celowo powodują wypadki i wyłudzają odszkodowania. Jak nie dać się złapać?
+Haker przyznał, jaki najgorszy błąd popełniamy, korzystając z sieci
+Fińska prasa o odpowiedzialności za Holokaust. Historia zawróconego statku z Żydami
+Płatne ładowanie aut elektrycznych na stacjach Lotosu - 24 zł za...
+Używane Volvo V40 – czy można mu zaufać?
+Edward Paczkowski. Rom, który przeżył zagładę
+"Mówiono, że helikopter to fanaberia Bryanta. Niewielu zna prawdę"
+Brexit tuż za rogiem. Uwaga! Ważne zmiany po 31 stycznia
+Nowości Mercedesa na ten rok: nie tylko nowe SUV-y
+Boeing 737 MAX – historia samolotu pasażerskiego "zaprojektowanego przez klaunów"
+Skrzynecka zrzuciła 16 kg, chudną też Gessler i Kozidrak. Jakie diety stosują? Wiemy to!
+Była małą dziewczynką, gdy zginęła Ewa Sałacka. Jak wygląda dziś córka aktorki?
+Patryk Vega zaapelował do policji w sprawie premiery filmu "Bad Boy". Komenda Główna odpowiedziała
+Turyści czują się bezkarni i mimo zakazów zbaczają ze szlaków
+Siedem przepisów na pyszne obiady na cały tydzień. Zacznij od zupy!
+VOD.PL
+Cicha nocPavarottiSerialeFilmy za darmo
+Szczepionki przeciw grypie to mit? Ekspert wyjawia całą prawdę
+Śmigłowiec, który jest dumą naszej armii. Zagraniczne wojska ustawiają się po niego w kolejce
+W tym mieście rządzą kapłani. "Trzeba sympatyzować z PiS-em, inaczej nie ma szans"
+Samolot miał już wracać do Warszawy. Wtedy wstała pijana pasażerka. A to dopiero początek problemów
+Który samochód jest zaparkowany prawidłowo? Nawet wieloletni kierowcy mają z tym problem
+Gdy zniknęła 18-latka, wszyscy myśleli, że to zaginięcie. Po latach przyjaciółka jej chłopaka wyjawiła prawdę
+Jak dobrze znasz mapę Polski? Zobacz, czy odpowiesz na wszystkie pytania
+Ile piją Polacy? Ostatni ranking mówi wiele. Na tle światowej czołówki wypadamy źle
+WARSZAWA 
+CBA w urzędzie wojewódzkim. Chodzi o łapówki byłego burmistrza Włoch i budowę hotelu
+Jaka pogoda we wtorek? Sprawdź prognozę
+Uwaga! TVN: dwuletni Kajtek potrzebuje rodziny 
+Otwarcie Park of Poland 20 lutego
+Szpital dla jeży. Leczą kolczastych pacjentów
+Jaki jest dziś stan powietrza w Warszawie?
+Sprawdź nowe oferty. Znajdź pracę marzeń STEPSTONE
+FILM, TV, MUZYKA, KSIĄŻKI
+"Leśna Muzyka" Dawida Podsiadły w NOSPR: na martwej planecie muzyki nie będzie [RELACJA]
+"Wiedźmin". Jakie sceny usunięto z serialu?
+Remigiusz Mróz podpadł wójtowi gminy Białopole
+We Wrocławiu obrabowano willę Tokarczuk
+Grammy 2020. Oto laureaci muzycznych Oscarów!
+Polski Pirelli: kobieta oczami Szyca i Podsiadło
+TVN opóźnia start ramówki wiosną. Jest komentarz
+Muzyka z "Wiedźmina" podbija listy przebojów
+Selena Gomez: byłam ofiarą znęcania psychicznego
+CO W TV? "Szczęśliwy Lazzaro" i "Solaris"
+Więcej:
+MuzykaProgram TVFilmKulturaKino
+TECHNOLOGIE I GRY
+Promocja na sprzęt w Biedronce - m.in. drukarka
+Polska kupuje F-35. Co wiemy o tych maszynach?
+TV za 400 zł w sklepach. Ogromne zainteresowanie
+Aplikacja Revolut kontra banki. Co się opłaca?
+NASA pokazuje globalne skutki pożarów w Australii
+Gra Captain Tsubasa na nowych materiałach na PS4
+Ilu ludzi stanęło na Księżycu? O tym mało kto wie 
+Londyn zainstaluje kamery do skanowania twarzy
+Google Maps mają być jeszcze lepsze dzięki AI
+Typy i rodzaje słuchawek. Oto, co trzeba wiedzieć
+Przy Cyberpunk 2077 pracuje drugie polskie studio
+Największy odrzutowiec dwusilnikowy po testach
+Więcej:
+Newsletter - zapisz się i bądź na bieżąco!Gorące:
+Komputerswiat.plPCLab.plGamezillaVerdyktDownloadNo Fluff Jobs
+MOTORYZACJA
+Zima stulecia dała w kość kierowcom
+Audi Q3 i Q5 - w niewiarygodnie niskich cenach
+Kolizja ze sprawcą bez OC - czy jest szansa na jakieś odszkodowanie?
+Renault Captur - uczeń przerósł mistrza? 
+Wszystkie nowości Mercedesa na 2020 rok 
+Mercedes GLC 200 - hybryda nie dla oszczędnych 
+BMW nadal płoną - jak idzie akcja serwisowa? 
+Używane Volvo V40 – czy można mu zaufać? 
+Jak dbać o hamulce?
+Nowe Skody tańsze nawet o 12 000 zł
+Więcej:
+Co warto kupić?AktualnościGorące:
+Testy aut nowychPorady
+PODRÓŻE
+Najsłynniejszy blok w Polsce. Chcielibyście w nim mieszkać?
+Przetestowaliśmy wszystkie ośrodki narciarskie w Polsce - sprawdźcie wyniki!
+Właściciele stoków dopłacą do interesu? "Takiego sezonu nie było od lat"
+Dlaczego ludzie boją się latać samolotem?
+W Polsce powstał największy park wodny w Europie. Wiemy, kiedy otwarcie
+Kiedy najłatwiej złapać wirusa w samolocie?
+Więcej:
+Zabytki UNESCOPolskaWeekendPlażeGóryNartyPoradyCo warto kupić?Gorące:
+Onet On Tour w IrlandiiCity On Tour w Pilznie
+NOIZZ
+Jestem gejem. Gdybym urodził się przed wojną, trafiłbym do obozu z różowym trójkątem
+Artur Rojek wraca po 6 latach z albumem "Kundel" i rusza w trasę koncertową
+To Polacy stoją za projektem Muzeum Wulkanu na Islandii. Przypomina UFO
+Możesz być, kim chcesz - o co chodzi w memie #DollyPartonChallenge z czterema zdjęciami
+Samochód elektryczny, który ma być jak sneakersy. Widzieliśmy Volvo XC40 Recharge
+"Droga koszykówko" - za ten film o miłości do gry Kobe Bryant dostał Oscara
+OFERTY
+Sekcja we współpracy ze Skąpiec.pl
+Jaką karmę podawać rasowym psom?
+OFERTA DNIA
+Islandia - wyjedź na spotkanie z zorzą
+Lustrzanki - zobacz, jakimi aparatami interesują się teraz klienci
+Marek Cekaw
+Pięć akcesoriów kosmetycznych, które naprawdę warto mieć
+Świetna promocja na zaawansowany telewizor - można oszczędzić niemal tysiąc złotych!
+Zobacz, który sprzęt z 2019 roku kupisz w tym roku taniej
+Jak sobie radzić z brakiem miejsca na segregowanie odpadów?
+To nie jest klimat dla opon zimowych? Całoroczne modele mogą być lepszym wyborem
+Jak zrobić domowy jogurt? Z tymi urządzeniami to proste!
+Damskie ubrania z sezonowych wyprzedaży, które będą modne wiosną 2020
+Mieszkanie w kolorze roku Pantone - classic blue
+WYPOŻYCZALNIA FILMÓW
+PremieryKlasyki kinaDisneyWersal. Prawo krwiBezpłatneKomedieSensacyjne
+PREMIERA
+Ból i blask
+Dramat
+Ad Astra
+Science Fiction
+HIT
+Polityka
+Polityczny
+Złodziej i oszustka
+Kryminał
+PREMIERA
+Sługi wojny
+Kryminał
+Tajemnica Anioła
+Thriller
+ZA DARMO
+Instynkt matki
+Thriller
+VOD.PL - OGLĄDAJ WIĘCEJ
+Objaw, który wielu Polaków często bagatelizuje. Świadczy o ciężkiej chorobie serca
+CIA ukrywało nazistowskiego zbrodniarza. "Odwalał brudną robotę"
+Zobacz film, który był pierwowzorem "Gladiatora". Lepszy niż dzieło z Russelem Crowem?
+Zobacz jubileuszowy 3000 odcinek "Pierwszej miłości" za darmo!
+ZNAJDŹ PRACĘ
+ITFinanseSprzedażLogistykaPraca inżynieryjna i technicznaBankowość i ubezpieczenia
+Szukasz aktualnych ofert pracy? Sprawdź na Stepstone.pl
+Chcesz pracować w sprzedaży? Sprawdź nasze oferty
+Praca w IT? Porównaj najnowsze oferty i aplikuj
+Zobacz nasze oferty i znajdź pracę w bankowości i usługach finansowych
+UMÓW SIĘ NA RANDKĘ
+Co mówi o nas kawa, jaką pijemy? [infografika]
+Mężczyźni kochają patrzeć na takie kobiety. Wydają im się bardziej atrakcyjne
+Co mężczyźni lubią w seksie? Oto pięć największych hitów
+Ideał kobiecości - tak zmieniał się przez wieki [infografika]
+ONET PRO BONO
+Dramatyczna walka z czasem o zdrowie Marysi
+W rok naprawili i podarowali potrzebującym 15 tys. par butów
+Weź udział w licytacji i zjedz obiad w szczytnym celu w towarzystwie aktorki Katarzyny Herman
+Chcesz otrzymać większy zwrot podatku? Pamiętaj, żeby odliczyć darowizny
+NAJLEPSZE GRY ONLINE! GRAJ ZA DARMO W GAMEPLANET
+Jaki jest twój szczęśliwy numer? Zgarnij kasę dla nowych graczy!
+Apokalipsa zaczyna się w pociągu... Graj za darmo
+Strategiczna gra planszowa, która nie wybacza błędów…
+Zostań mistrzem skoków! Graj za darmo!
+Mapa serwisówPrywatnośćReklamaPraca w Onecie© 2020 Ringier Axel Springer Polska sp. z o.o. - Developed by DreamLab
+Prawie gotowe!
+Kliknij w guzik „Zezwalaj”, żeby włączyć interesujące Cię powiadomieniaDzięki, teraz zawsze będziesz na bieżąco.Dzięki, możesz zarządzać powiadomieniami w każdej chwili, klikając tutaj lub wchodząc na stronę www.onet.pl/#powiadomienia
+Chcesz otrzymywać powiadomienia?
+Gotowe!
+ZAPISZ
+Dzięki! Nie zapomnij się zapisać na interesujące Cię tematy!
+`;
+
+const isValidChar = char => char.match(/[a-zA-Z0-9]/);
+
+const instanceCounter = (count, char) => {
+  count[char] ? (count[char] += 1) : (count[char] = 1);
+  return count;
+};
+
+const result = text
+  .split('')
+  .filter(isValidChar)
+  .map(char => char.toLowerCase())
+  .reduce(instanceCounter, {});
+
+console.log(result);
