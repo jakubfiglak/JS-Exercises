0690d33a818ce3a91423893e04fff34ca6e72d3b
Jakub Figlak <jakub.figlak@gmail.com>
2020-02-01 14:00:38 +0100

    Finish exercise no. 8 - shopping list

--- 08-shopping-list/index.js
+++ 08-shopping-list/index.js
@@ -1,9 +1,7 @@
-const input = document.querySelector('#shopping-input');
-const addBtn = document.querySelector('#add-btn');
 const form = document.querySelector('.shopping-form');
 const list = document.querySelector('.list');
 
-const items = [];
+let items = [];
 
 function createItem(e) {
   e.preventDefault();
@@ -16,6 +14,7 @@ function createItem(e) {
   };
 
   items.push(item);
+  form.reset();
 
   list.dispatchEvent(new CustomEvent('itemsUpdated'));
 }
@@ -26,9 +25,13 @@ function displayItems() {
       item =>
         `
         <li class="list-element">
-            <input type="checkbox" />
+            <input type="checkbox" value="${item.id}" ${item.checked &&
+          'checked'}/>
             <p class="list-description">${item.itemName}</p>
-            <button class="btn btn-round">
+            <button 
+            class="btn btn-round" 
+            value="${item.id}" 
+            aria-label="delete ${item.itemName}">
               <i class="fas fa-trash-alt"></i>
             </button>
           </li>
@@ -36,8 +39,48 @@ function displayItems() {
     )
     .join('');
 
+  console.log('displaying items...');
   list.innerHTML = html;
+}
+
+const saveToLocalStorage = () =>
+  localStorage.setItem('items', JSON.stringify(items));
+
+const getFromLocalStorage = () => {
+  const lsItems = JSON.parse(localStorage.getItem('items'));
+  console.log(lsItems);
+
+  if (lsItems.length) {
+    items.push(...lsItems);
+    console.log('getting data from local storage...');
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+  }
+};
+
+function markAsChecked(id) {
+  const itemRef = items.find(item => item.id === id);
+  itemRef.checked = !itemRef.checked;
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+
+function deleteItem(id) {
+  items = items.filter(item => item.id !== id);
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
 }
 
 form.addEventListener('submit', createItem);
 list.addEventListener('itemsUpdated', displayItems);
+list.addEventListener('itemsUpdated', saveToLocalStorage);
+list.addEventListener('click', e => {
+  const id = parseInt(e.target.value);
+  console.log(id);
+
+  if (e.target.matches('input[type="checkbox"]')) {
+    markAsChecked(id);
+  }
+
+  if (e.target.matches('button')) {
+    deleteItem(id);
+  }
+});
+getFromLocalStorage();
